50651bce5085e1c0d99d3d33ded4dafdb7690840
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-14 14:47:23 -0600

    commit tree 20fa79ed485871ac8076bb321a319c7c226afe88

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
-# your code here
+class Stack
+    attr_accessor :stack
+    attr_reader :limit
+
+    def initialize(limit = nil)
+        @limit = limit
+        @stack = []
+    end
+
+    def length
+        stack.length
+    end
+
+    def push(value)
+        return stack[length] = value unless full?
+        raise "Stack Overflow"
+    end
+
+    def pop
+        stack.delete_at(length - 1)
+    end
+
+    def peek
+        stack[-1]
+    end
+
+    def size
+        self.length
+    end
+
+    def empty?
+        length == 0
+    end
+
+    def full?
+        !!(limit && length >= limit)
+    end
+
+    def search(value)
+        index = stack.rindex(value)
+        return length - index - 1 if index
+        -1
+    end
+end
